539c67ad54b7cb72d1cfa3c3dbed5de949e2acfe
T-Kay <ytlol@live.jp>
2016-10-22 11:57:44 +0900

    add proxy log

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -154,8 +154,8 @@ class SlackListener < Redmine::Hook::Listener
 			client = HTTPClient.new
 			client.ssl_config.cert_store.set_default_paths
 			client.ssl_config.ssl_version = :auto
-			client.proxy = ENV["FIXIE_URL"]
 			client.post_async url, {:payload => params.to_json}
+			logger "Send data #{pp params}"
 		rescue Exception => e
 			logger.warn("cannot connect to #{url}")
 			logger.warn(e)
