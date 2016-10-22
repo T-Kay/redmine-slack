7ddb2c34f546fb0f54d0fd9cd5be0e30191d831d
T-Kay <ytlol@live.jp>
2016-10-22 13:59:51 +0900

    set log

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -155,10 +155,9 @@ class SlackListener < Redmine::Hook::Listener
 			client.ssl_config.cert_store.set_default_paths
 			client.ssl_config.ssl_version = :auto
 			client.post_async url, {:payload => params.to_json}
-			logger "Send data #{pp params}"
 		rescue Exception => e
-			logger.warn("cannot connect to #{url}")
-			logger.warn(e)
+			logger "cannot connect to #{url}"
+			logger "#{e}"
 		end
 	end
 
